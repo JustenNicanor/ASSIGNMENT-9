fa064cfc33c6a15085a2ce9230920445eb82ba60
JustenNicanor <92636904+JustenNicanor@users.noreply.github.com>
2022-02-09 18:53:12 +0800

    Fourth commit(Final! Coding educational background, adjusting sizes)

--- Assignment 9(1).py
+++ Assignment 9(1).py
@@ -6,9 +6,7 @@ with open('Personaldetails.json') as jsonfile:
 Adresss = Information["PERSONAL INFORMATION"]["Add"]
 Contact =Information["PERSONAL INFORMATION"]["Contact"]
 Email = Information["PERSONAL INFORMATION"]["Email"]
-
 Personal = Information["PERSONAL INFORMATION"]["Perinfo"]
-
 DateofBirth	= Information["PERSONAL INFORMATION"]["Date of Birth"]
 PlaceofBirth = Information["PERSONAL INFORMATION"]["Place of Birth"]
 Gender = Information["PERSONAL INFORMATION"]["Gender"]		
@@ -21,6 +19,12 @@ Ocupation = Information["PERSONAL INFORMATION"]["Ocupation"]
 MothersName = Information["PERSONAL INFORMATION"]["Mother`s Name"]
 Occupation	= Information["PERSONAL INFORMATION"]["Occupation"]	
 Address	= Information["PERSONAL INFORMATION"]["Address"]
+Educational = Information["PERSONAL INFORMATION"]["Education"]
+Elem = Information["PERSONAL INFORMATION"]["Elem"]
+Junior = Information["PERSONAL INFORMATION"]["JHigh"]
+Senior = Information["PERSONAL INFORMATION"]["SHigh"]
+College = Information["PERSONAL INFORMATION"]["College"]
+Course = Information["PERSONAL INFORMATION"]["Course"]
 
 pdf = FPDF('p', 'mm', 'Letter')
 pdf.add_page()
@@ -31,22 +35,27 @@ pdf.cell(50, 5, Adresss, ln=True)
 pdf.cell(50, 5, Contact, ln=True)
 pdf.cell(50, 5, Email, ln=True)
 pdf.set_font('times', 'B', 15)
-pdf.cell(50, 10, Personal, ln=True)
+pdf.cell(50, 15, Personal, ln=True)
 pdf.set_font('times', '', 10)
-pdf.cell(50, 5, DateofBirth, ln=True)
-pdf.cell(50, 5, PlaceofBirth, ln=True)
-pdf.cell(50, 5, Gender, ln=True)
-pdf.cell(50, 5, CivilStatus, ln=True)
-pdf.cell(50, 5, Citizenship, ln=True)
-pdf.cell(50, 5, Height, ln=True)
-pdf.cell(50, 5, Weight, ln=True)
-pdf.cell(50, 5, FathersName, ln=True)
-pdf.cell(50, 5, Ocupation, ln=True)
-pdf.cell(50, 5, MothersName, ln=True)
-pdf.cell(50, 5, Occupation, ln=True)
-pdf.cell(50, 5, Address, ln=True)
-
-
-
+pdf.cell(50, 10, DateofBirth, ln=True)
+pdf.cell(50, 10, PlaceofBirth, ln=True)
+pdf.cell(50, 10, Gender, ln=True)
+pdf.cell(50, 10, CivilStatus, ln=True)
+pdf.cell(50, 10, Citizenship, ln=True)
+pdf.cell(50, 10, Height, ln=True)
+pdf.cell(50, 10, Weight, ln=True)
+pdf.cell(50, 10, FathersName, ln=True)
+pdf.cell(50, 10, Ocupation, ln=True)
+pdf.cell(50, 10, MothersName, ln=True)
+pdf.cell(50, 10, Occupation, ln=True)
+pdf.cell(50, 10, Address, ln=True)
+pdf.set_font('times', 'B', 15)
+pdf.cell(50, 15, Educational, ln=True)
+pdf.set_font('times', '', 10)
+pdf.cell(50, 10, Elem, ln=True)
+pdf.cell(50, 10, Junior, ln=True)
+pdf.cell(50, 10, Senior, ln=True)
+pdf.cell(50, 10, College, ln=True)
+pdf.cell(50, 10, Course, ln=True)
 
 pdf.output('NICANOR_JUSTEN.pdf')
