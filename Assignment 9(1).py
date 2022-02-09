82fb9e57a056acddae31786281521ebf83b4f5ee
JustenNicanor <92636904+JustenNicanor@users.noreply.github.com>
2022-02-09 17:00:20 +0800

    Third commit(Revising my personal info, Coding my personal info, coding educational background to json)

--- Assignment 9(1).py
+++ Assignment 9(1).py
@@ -7,6 +7,8 @@ Adresss = Information["PERSONAL INFORMATION"]["Add"]
 Contact =Information["PERSONAL INFORMATION"]["Contact"]
 Email = Information["PERSONAL INFORMATION"]["Email"]
 
+Personal = Information["PERSONAL INFORMATION"]["Perinfo"]
+
 DateofBirth	= Information["PERSONAL INFORMATION"]["Date of Birth"]
 PlaceofBirth = Information["PERSONAL INFORMATION"]["Place of Birth"]
 Gender = Information["PERSONAL INFORMATION"]["Gender"]		
@@ -15,7 +17,7 @@ Citizenship	= Information["PERSONAL INFORMATION"]["Citizenship"]
 Height = Information["PERSONAL INFORMATION"]["Height"]		
 Weight = Information["PERSONAL INFORMATION"]["Weight"]
 FathersName	= Information["PERSONAL INFORMATION"]["Father`s Name"]
-Occupation = Information["PERSONAL INFORMATION"]["Ocupation"]
+Ocupation = Information["PERSONAL INFORMATION"]["Ocupation"]
 MothersName = Information["PERSONAL INFORMATION"]["Mother`s Name"]
 Occupation	= Information["PERSONAL INFORMATION"]["Occupation"]	
 Address	= Information["PERSONAL INFORMATION"]["Address"]
@@ -28,6 +30,22 @@ pdf.set_font('times', '', 10)
 pdf.cell(50, 5, Adresss, ln=True)
 pdf.cell(50, 5, Contact, ln=True)
 pdf.cell(50, 5, Email, ln=True)
+pdf.set_font('times', 'B', 15)
+pdf.cell(50, 10, Personal, ln=True)
+pdf.set_font('times', '', 10)
+pdf.cell(50, 5, DateofBirth, ln=True)
+pdf.cell(50, 5, PlaceofBirth, ln=True)
+pdf.cell(50, 5, Gender, ln=True)
+pdf.cell(50, 5, CivilStatus, ln=True)
+pdf.cell(50, 5, Citizenship, ln=True)
+pdf.cell(50, 5, Height, ln=True)
+pdf.cell(50, 5, Weight, ln=True)
+pdf.cell(50, 5, FathersName, ln=True)
+pdf.cell(50, 5, Ocupation, ln=True)
+pdf.cell(50, 5, MothersName, ln=True)
+pdf.cell(50, 5, Occupation, ln=True)
+pdf.cell(50, 5, Address, ln=True)
+
 
 
 
